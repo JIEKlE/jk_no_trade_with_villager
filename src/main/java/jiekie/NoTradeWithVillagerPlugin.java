b19caf3c4b094d18d1877564955ce213d1429dd7
BAEKJlHYE <bjiekie@gmail.com>
2025-04-21 17:37:28 +0900

    add copyright

--- src/main/java/jiekie/NoTradeWithVillagerPlugin.java
+++ src/main/java/jiekie/NoTradeWithVillagerPlugin.java
@@ -8,6 +8,9 @@ public final class NoTradeWithVillagerPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new PlayerEvent(this), this);
+
+        getLogger().info("거래 금지 플러그인 by Jiekie");
+        getLogger().info("Copyright © 2025 Jiekie. All rights reserved.");
     }
 
     @Override
